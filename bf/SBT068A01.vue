c517713b43d52cbb64eb04718e439e634f55fe68
DeniceKim <18107365+DeniceKim@users.noreply.github.com>
2025-12-04 17:09:54 +0900

    doc

---
+++ bf/SBT068A01.vue
@@ -0,0 +1,39 @@
+<route lang="yaml">
+meta:
+  id: SBT068A01
+  title: 앱테크
+  menu: 혜택 > 앱테크 메인화면
+  layout: SubLayout
+  category: 혜택
+  publish: 김대민
+  publishVersion: 0.8
+  status: 작업중
+  header:
+    variant: sub
+    fixed: true
+    back: true
+    close: false
+    home: true
+  appClassList: "sc-bf__apptech"
+  mainClassList: "bf-apptech__submain"
+</route>
+<template>
+  <!-- 콘텐츠 영역 -->
+  <div class="sc-contents__body">
+    <section aria-label="외부 광고 배너">
+    </section>
+    <section aria-label="오늘의 앱테크">
+    </section>
+    <section aria-label="오늘의 미션">
+    </section>
+    <!-- 어드민 관리 -->
+    <section aria-label="포인트 눌러서 바로 적립">
+      <article aria-label="인라이플 배너"></article>
+      <article aria-label="애드팝콘"></article>
+    </section>
+    <section aria-label="포인트 더 받기">
+    </section>
+  </div>
+</template>
+<script setup>
+</script>
